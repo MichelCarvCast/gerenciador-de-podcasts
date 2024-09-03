83093921a118a7d3f7c19ced9d7655a34ece122b
MichelCarvCast <02michelc@gmail.com>
2024-09-02 22:05:39 -0300

    Criando o server http

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-console.log("hello World");
-console.log("Git automático teste");
+import * as http from 'http';
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {});
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}`);
+});
